5265b29342b3ea8e87af6e5e1ea54b93ce092eb5
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-01-10 19:29:13 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware para manejar JSON
+app.use(express.json());
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Hola, Mundo!');
+});
+
+// Ruta de ejemplo
+app.get('/api', (req, res) => {
+    res.json({ mensaje: 'Este es un endpoint simple.' });
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
